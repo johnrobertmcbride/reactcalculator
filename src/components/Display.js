cb7a08672ba49e00d3679cfd565193808ca1c35e
John McBride <john@alternatelabs.co>
2018-10-16 10:06:19 +0100

    add styled components

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledDisplay = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  width: 400px;
+  height: 150px;
+  border: 1px solid black;
+  font-size: 75px;
+  color: white;
+  background-color: black;
+`
 
 class Display extends Component {
   render() {
     return (
-      <div className="display">
+      <StyledDisplay>
         {this.props.display}
-      </div>
+      </StyledDisplay>
     )
   }
 }
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyledSquare = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  background-color: lightgray;
+`
 
 class Square extends Component {
   render() {
     return (
-      <div className="square" onClick={() => this.props.onClick()}>
+      <StyledSquare onClick={() => this.props.onClick()}>
         {this.props.value}
-      </div>
+      </StyledSquare>
     )
   }
 }
